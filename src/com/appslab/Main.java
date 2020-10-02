bfec2d79c1e539e73c531da5d753fd9f59669c91
unknown <roan.kalab@gmail.com>
2020-10-02 13:19:26 +0200

    Hotovo

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,66 @@
 package com.appslab;
+import java.util.Scanner;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+        int totalSlices;
+        int numberOfRecipients;
+        int slicesEach;
+        boolean equality;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Total slices: ");
+        totalSlices = input.nextInt();
+        System.out.print("Number of recipients: ");
+        numberOfRecipients = input.nextInt();
+        System.out.print("Slices each: ");
+        slicesEach = input.nextInt();
+
+        equality = equalSlices(totalSlices, numberOfRecipients, slicesEach);
+
+        System.out.println();
+
+        if (equality)
+        {
+            if (slicesEach >= 1)
+            {
+                if (slicesEach == 1)
+                    System.out.print("Everyone can get " + slicesEach + " slice.");
+                else
+                {
+                    System.out.print("Everyone can get " + slicesEach + " slices.");
+                }
+            }
+            else
+            {
+                System.out.print("No one gets nothing.");
+            }
+        }
+        else
+        {
+            if (slicesEach >= 1)
+            {
+                if (slicesEach == 1)
+                    System.out.print("Not everyone can get " + slicesEach + " slice.");
+                else
+                {
+                    System.out.print("Not everyone can get " + slicesEach + " slices.");
+                }
+            }
+            else
+            {
+                System.out.print("No one gets nothing.");
+            }
+        }
+
+
+
+    }
+    public static boolean equalSlices(int totalSlices, int numberOfRecipients, int slicesEach)
+    {
+        return numberOfRecipients * slicesEach <= totalSlices;
     }
 }
