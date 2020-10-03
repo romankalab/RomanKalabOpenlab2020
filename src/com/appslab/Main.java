8206475639b9d67a0f0abfc1153a306d63f507bc
unknown <roan.kalab@gmail.com>
2020-10-03 22:43:41 +0200

    Hotovo

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,24 @@
 package com.appslab;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+        Student student1 = new Student();
+        Student student2 = new Student();
+
+        student1.name = "John";
+        student1.studentNumber = 2;
+        student1.phoneNumber = 123456;
+        student1.address = "Legionarska 1, 01001 Zilina";
+
+        student2.name = "Sam";
+        student2.studentNumber = 5;
+        student2.phoneNumber = 456789;
+        student2.address = "Legionarska 2, 01001 Zilina";
+
+        System.out.println(student1.name + ", " + student1.studentNumber + ", " + student1.phoneNumber + ", " + student1.address);
+        System.out.print(student2.name + ", " + student2.studentNumber + ", " + student2.phoneNumber + ", " + student2.address);
     }
 }
---
+++ src/com/appslab/Student.java
@@ -0,0 +1,9 @@
+package com.appslab;
+
+public class Student
+{
+    String name;
+    int studentNumber;
+    int phoneNumber;
+    String address;
+}
