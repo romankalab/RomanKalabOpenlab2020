c290fec78713bd6b549b52ee6feba9981bd22f19
unknown <roan.kalab@gmail.com>
2020-10-03 11:26:16 +0200

    Hotovo

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -15,26 +15,60 @@ public class Main
 
         char[] wordToChars = word.toCharArray();
 
+        isItPalindrome = palindromeIdentify(wordToChars);
+
+        System.out.println();
+        if (isItPalindrome)
+        {
+            System.out.print("Word " + word + " is a palindrome.");
+        }
+        else
+        {
+            System.out.print("Word " + word + " isn't a palindrome.");
+        }
+
     }
-    public static boolean palindromeIdentify(String word, char[] wordToChars)
+    public static boolean palindromeIdentify(char[] wordToChars)
     {
         int i = 0;
-        int x = 0;
+        int x1 = 0;
+        int x2;
+        StringBuilder firstHalf = new StringBuilder();
+        StringBuilder secondHalf = new StringBuilder();
+        char addChar1;
+        char addChar2;
         for (Character character : wordToChars)
             i++;
         if (i % 2 == 0)
         {
             i++; // +1 gives real amount of chars in array
-            String firstHalf = new String();
-            char addChar;
+            x2 = i - 2;
             for (i = i / 2; i >= 0; i--)
             {
-                addChar = wordToChars[x];
-                firstHalf = firstHalf + addChar;
-                x++;
+                addChar1 = wordToChars[x1];
+                addChar2 = wordToChars[x2];
+                firstHalf.append(addChar1);
+                secondHalf.append(addChar2);
+                x1++;
+                x2--;
             }
+            return firstHalf.toString().equals(secondHalf.toString());
 
         }
-        return true;
+        else
+        {
+            i++; // +1 gives real amount of chars in array
+            x2 = i - 2;
+            for (i = (i - 1) / 2; i >= 0; i--)
+            {
+                addChar1 = wordToChars[x1];
+                addChar2 = wordToChars[x2];
+                firstHalf.append(addChar1);
+                secondHalf.append(addChar2);
+                x1++;
+                x2--;
+            }
+            return firstHalf.toString().equals(secondHalf.toString());
+        }
     }
 }
