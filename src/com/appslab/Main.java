e4ebab82d9749d78a73aaa016d681a22c17831dd
unknown <roan.kalab@gmail.com>
2020-10-03 17:01:08 +0200

    Hotovo

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,63 @@
 package com.appslab;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        Scanner input = new Scanner(System.in);
+        String memory1;
+        String memory2;
+        int result;
+
+        System.out.print("rock, paper, scissors");
+        System.out.println();
+        System.out.print("Player1: ");
+        memory1 = input.nextLine();
+        System.out.print("Player2: ");
+        memory2 = input.nextLine();
+
+        result = rpsGame(memory1, memory2);
+
+        System.out.println();
+        if (result == 0)
+            System.out.print("TIE.");
+        if (result == 1)
+            System.out.print("Player1 wins.");
+        if (result == 2)
+            System.out.print("Player2 wins.");
+        if (result == 3)
+            System.out.print("An error occurred.");
+    }
+    public static int rpsGame(String memory1, String memory2)
+    {
+        if (memory1.equals("rock"))
+        {
+            if (memory2.equals("rock"))
+                return 0;
+            if (memory2.equals("paper"))
+                return 2;
+            if (memory2.equals("scissors"))
+                return 1;
+        }
+        if (memory1.equals("paper"))
+        {
+            if (memory2.equals("rock"))
+                return 1;
+            if (memory2.equals("paper"))
+                return 0;
+            if (memory2.equals("scissors"))
+                return 2;
+        }
+        if (memory1.equals("scissors"))
+        {
+            if (memory2.equals("rock"))
+                return 2;
+            if (memory2.equals("paper"))
+                return 1;
+            if (memory2.equals("scissors"))
+                return 0;
+        }
+        return 3;
     }
 }
