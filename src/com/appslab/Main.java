a2172fef11c3a3eb49058858da4850d8aca76c68
romankalab <roan.kalabgmail.com>
2020-11-04 19:10:39 +0100

    save

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,15 @@
 package com.appslab;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5));
+        for (int x : list)
+        {
+            System.out.print(x + ", ");
+        }
     }
 }
