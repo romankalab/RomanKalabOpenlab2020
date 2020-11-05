6a3e4bae5b76651baf7883b700fd74fbc0fc82c5
romankalab <roan.kalabgmail.com>
2020-11-05 22:22:36 +0100

    hotovo

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,21 @@
 package com.appslab;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        int newElement = 9;
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 3, 4, 5, 5, 6, 7));
+        addIfNotExists(newElement, list);
+        list.forEach(System.out::println);
+    }
+    static void addIfNotExists (int newElement, ArrayList<Integer> list)
+    {
+        if (!list.contains(newElement))
+        {
+            list.add(newElement);
+        }
     }
 }
