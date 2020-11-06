50f1e1b3a7f356fde7d23e1a48954cba484b6f4f
romankalab <roan.kalabgmail.com>
2020-11-06 15:23:15 +0100

    hotovo

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,18 @@
 package com.appslab;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
+        list.forEach(number -> {if (number % 2 == 1) oddNumbers.add(number);});
+        list = oddNumbers;
+        list.forEach(System.out::println);
+
     }
 }
