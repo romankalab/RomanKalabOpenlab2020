d5bc289905156894c9736760c5ac9721d124e2ee
romankalab <roan.kalabgmail.com>
2020-11-11 12:13:18 +0100

    uprava

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -9,9 +9,7 @@ public class Main
     public static void main(String[] args)
     {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-        ArrayList<Integer> oddNumbers = new ArrayList<>();
-        list.forEach(number -> {if (number % 2 == 1) oddNumbers.add(number);});
-        list = oddNumbers;
+        list.removeIf(number -> number % 2 == 0);
         list.forEach(System.out::println);
 
     }
