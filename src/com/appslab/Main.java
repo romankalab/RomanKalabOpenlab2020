729d9d66241d52dca7fe62706fb0d53dd8b71c4c
romankalab <roan.kalabgmail.com>
2020-11-18 15:47:00 +0100

    update

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,15 +2,18 @@ package com.appslab;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         list.removeIf(number -> number % 2 == 0);
         list.forEach(System.out::println);
+
+        // list = list.stream().filter(number -> number % 2 == 1).collect(Collectors.toList());
 
     }
 }
