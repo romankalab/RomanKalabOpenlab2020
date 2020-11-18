33ecb5351454d7d2889fc76f714e2557b08a35f3
romankalab <roan.kalabgmail.com>
2020-11-18 16:03:05 +0100

    Update to stream

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,19 +1,23 @@
 package com.appslab;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        Person[] people = new Person[] {
-                new Person("Jack", 20, 5480),
-                new Person("Sam", 22, 6770),
-                new Person("Dan", 25, 4590),
-                new Person("Josh", 19, 3550),
-        };
-        int sum = 0;
-        for(Person person : people)
-        {
-            sum = sum + person.getBudget();
-        }
-        System.out.print(sum);
+        Person Jack = new Person("Jack", 20, 5480);
+        Person Sam = new Person("Sam", 22, 6770);
+        Person Dan = new Person("Dan", 25, 4590);
+        Person Josh = new Person("Josh", 19, 3550);
+        List<Person> people = Arrays.asList(Jack, Sam, Dan, Josh);
+
+        System.out.println(people.stream().mapToInt(Person::getBudget).sum());
+
+        // int sum = 0;
+        // for(Person person : people)
+        // {
+        //    sum = sum + person.getBudget();
+        // }
+        // System.out.print(sum);
     }
 }
