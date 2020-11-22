926ce0cb2918aed2aca4ca621c6c0bd81b1c0b71
romankalab <roan.kalabgmail.com>
2020-11-22 01:48:16 +0100

    hotovo

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,14 +2,20 @@ package com.appslab;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5));
-        if (list.isEmpty())
+        Set<Integer> set = new HashSet<>(list);
+        set.add(1);
+        set.forEach(System.out::println);
+
+        /* if (list.isEmpty())
             System.out.print("The list is empty.");
         else
-        list.forEach(System.out::println);
+        list.forEach(System.out::println); */
     }
 }
