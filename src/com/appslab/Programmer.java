eabc5d58b9bb5e813d4db83c14903fde17146941
romankalab <roan.kalabgmail.com>
2020-11-25 22:09:16 +0100

    hotovo

--- src/com/appslab/Programmer.java
+++ src/com/appslab/Programmer.java
@@ -5,4 +5,8 @@ public class Programmer extends Employee {
     public Programmer(float salary, int bonus) {
         super(salary, bonus, Employment.PROGRAMMER);
     }
+
+    public void getInfo() {
+        System.out.println(employment.job + "'s salary is " + (salary + bonus) + ".");
+    }
 }
