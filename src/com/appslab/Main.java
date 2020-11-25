47cb1661254fecab0106e61ae52926b87b6ee898
romankalab <roan.kalabgmail.com>
2020-11-25 22:24:13 +0100

    hotovo

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,25 @@
 package com.appslab;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Red", 1);
+        map.put("Green", 2);
+        map.put("Black", 3);
+        map.put("White", 4);
+        addIfDoesNotExist(map);
+        System.out.print(map);
+    }
+    public static void addIfDoesNotExist(HashMap<String, Integer> map) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter key: ");
+        String key = input.nextLine();
+        System.out.print("Enter value: ");
+        int value = input.nextInt();
+        map.putIfAbsent(key, value);
     }
 }
