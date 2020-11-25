78f39c86efcc936d225a9b483b02d6bd96534441
romankalab <roan.kalabgmail.com>
2020-11-25 21:24:52 +0100

    hotovo

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,15 @@
 package com.appslab;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        map.put("Red", 1);
+        map.put("Green", 2);
+        map.put("Black", 3);
+        map.put("White", 4);
+        System.out.println(map);
     }
 }
